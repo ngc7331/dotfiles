5bd057f07eef6faa07f2bc1a89be0d2a52aee79d
ngc7331 <ngc7331@outlook.com>
2024-05-21 22:42:49 +0800

    refactor(zsh): color-test.sh

--- zsh/color-test.sh
+++ zsh/color-test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-for color in {0..255}; do
+for color in {0..15}; do
   echo -e -n "\033[38;5;${color}m$(printf '%03d' ${color})\033[0m "
-    if [ $(((${color}) % 6)) -eq 3 ] && [ ${color} -ge 16 ]; then
-        echo
-    fi
-    if [ $(((${color}) % 8)) -eq 7 ] && [ ${color} -le 15 ]; then
-        echo
-    fi
+  if [ $(((${color}) % 8)) -eq 7 ]; then
+    echo
+  fi
 done
-echo
+
+for color in {16..255}; do
+  echo -e -n "\033[38;5;${color}m$(printf '%03d' ${color})\033[0m "
+  if [ $(((${color}) % 6)) -eq 3 ]; then
+    echo
+  fi
+done
