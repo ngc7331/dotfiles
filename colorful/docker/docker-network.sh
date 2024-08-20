d6e74cc326f40446587035bb9dc570f7ddbab9c5
ngc7331 <ngc7331@outlook.com>
2024-08-20 16:16:34 +0800

    refactor(colorful): move docker-related scripts to subdirectory

--- colorful/docker/docker-network.sh
+++ colorful/docker/docker-network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-source utils.sh
+source $(dirname $0)/../utils.sh
 
 NAME=${NAME:-colorful-br}
 SUBNET=${SUBNET:-21}
--- colorful/docker/portainer.sh
+++ colorful/docker/portainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-source utils.sh
+source $(dirname $0)/../utils.sh
 
 MIRROR=${MIRROR:-docker.io/}
 VERSION=${VERSION:-latest}
